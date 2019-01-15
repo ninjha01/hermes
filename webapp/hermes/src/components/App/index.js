6e92dbd510bb158e1387c3f2af454b0b8dfa65cb
Nishant Jha <ninjha01@gmail.com>
2019-01-15 13:29:43 -0500

    basic webapp

--- webapp/hermes/src/components/App/index.js
+++ webapp/hermes/src/components/App/index.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import { BrowserRouter as Router,
-	 Route,
-       } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Navigation from '../Navigation';
 import LandingPage from '../Landing';
-import UserPage from '../Users';
-import ExercisePage from '../Exercises';
-import AssesmentPage from '../Assesments';
+import SignUpPage from '../SignUp';
+import SignInPage from '../SignIn';
+import PasswordForgetPage from '../PasswordForget';
+import HomePage from '../Home';
+import AccountPage from '../Account';
+import AdminPage from '../Admin';
 
 import * as ROUTES from '../../constants/routes';
+import { withAuthentication } from '../Session';
 
 const App = () => (
-	<Router>
-	<div>
-	<Navigation/>
-	<Route exact path={ROUTES.LANDING} component={LandingPage} />
-	<Route path={ROUTES.USERS} component={UserPage} />
-	<Route path={ROUTES.EXERCISES} component={ExercisePage} />
-	<Route path={ROUTES.ASSESMENTS} component={AssesmentPage} />
-	</div>
-	</Router>
+  <Router>
+    <div>
+      <Navigation />
+
+      <hr />
+
+      <Route exact path={ROUTES.LANDING} component={LandingPage} />
+      <Route exact path={ROUTES.SIGN_UP} component={SignUpPage} />
+      <Route exact path={ROUTES.SIGN_IN} component={SignInPage} />
+      <Route
+        exact
+        path={ROUTES.PASSWORD_FORGET}
+        component={PasswordForgetPage}
+      />
+      <Route exact path={ROUTES.HOME} component={HomePage} />
+      <Route exact path={ROUTES.ACCOUNT} component={AccountPage} />
+      <Route exact path={ROUTES.ADMIN} component={AdminPage} />
+    </div>
+  </Router>
 );
 
-export default App;
+export default withAuthentication(App);
--- webapp/hermes/src/components/Firebase/firebase.js
+++ webapp/hermes/src/components/Firebase/firebase.js
@@ -1,18 +1,36 @@
 import app from 'firebase/app';
+import 'firebase/auth';
 
 const config = {
-    apiKey: "AIzaSyAGLKg2FKcZPuVNKwns-PSlTMQkpaiWqLU",
-    authDomain: "hermes-91802.firebaseapp.com",
-    databaseURL: "https://hermes-91802.firebaseio.com",
-    projectId: "hermes-91802",
-    storageBucket: "hermes-91802.appspot.com",
-    messagingSenderId: "245832035755"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
 };
 
 class Firebase {
-    constructor() {
-	app.initializeApp(config);
-    }
+  constructor() {
+    app.initializeApp(config);
+
+    this.auth = app.auth();
+  }
+
+  // *** Auth API ***
+
+  doCreateUserWithEmailAndPassword = (email, password) =>
+    this.auth.createUserWithEmailAndPassword(email, password);
+
+  doSignInWithEmailAndPassword = (email, password) =>
+    this.auth.signInWithEmailAndPassword(email, password);
+
+  doSignOut = () => this.auth.signOut();
+
+  doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+  doPasswordUpdate = password =>
+    this.auth.currentUser.updatePassword(password);
 }
 
 export default Firebase;
